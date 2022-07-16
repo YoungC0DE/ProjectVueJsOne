e8ee409cbc9f5e4ff59337092dbf74f7704f2f39
Rafael Anjos Santos <raphaelsantosajs@gmail.com>
2022-07-16 20:12:16 -0300

    beta

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './components/NavbarComponent.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).mount('#app')
